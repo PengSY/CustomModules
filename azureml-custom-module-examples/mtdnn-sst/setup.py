c909fe66b76f4fc65b7b7592f6a1dc951d573e24
PengSY <shipen@microsoft.com>
2019-08-21 16:59:08 +0800

    Add setup.py file.

---
+++ azureml-custom-module-examples/mtdnn-sst/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name="mtdnn-sst",
+    version="0.0.2",
+    description="MTDNN Fine Tuned Model for SST Task",
+    packages=["scripts"],
+    author="Shiyu Peng",
+    license="MIT",
+    include_package_data=True,
+)
