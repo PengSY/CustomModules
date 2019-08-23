adf300ee970a46ce12121f4e443052681562cc01
Shiyu Peng <shipen@microsoft.com>
2019-08-23 11:37:57 +0800

    Update setup.py to find sub-packages.

--- azureml-custom-module-examples/mtdnn-sst/setup.py
+++ azureml-custom-module-examples/mtdnn-sst/setup.py
@@ -1,11 +1,10 @@
-from setuptools import setup
-
+from setuptools import setup, find_packages
 
 setup(
     name="mtdnn-sst",
     version="0.0.2",
     description="MTDNN Fine Tuned Model for SST Task",
-    packages=["scripts"],
+    packages=find_packages(),
     author="Shiyu Peng",
     license="MIT",
     include_package_data=True,
